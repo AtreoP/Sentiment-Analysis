63d4cd81900ac9a3e6228259de057351e5cdebae
Atreo Pramanick <97874269+AtreoP@users.noreply.github.com>
2023-08-02 12:59:10 +0530

    Add files via upload

---
+++ sentiment.py
@@ -0,0 +1,24 @@
+
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sia = SentimentIntensityAnalyzer()
+
+###########################################################################
+
+def analyze_sentiment(text):
+    sentiment_score = sia.polarity_scores(text)['compound']
+    return sentiment_score
+
+###########################################################################
+
+sample_text = "{YOUR_STATEMENT}"
+sentiment_score = analyze_sentiment(sample_text)
+
+###########################################################################
+
+if sentiment_score >= 0.05:
+    print("Positive sentiment")
+elif sentiment_score <= -0.05:
+    print("Negative sentiment")
+else:
+    print("Neutral sentiment")
